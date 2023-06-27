8145cfef18c354c084e43f2d2ba4e56861577962
Yer Yang <yyhmg_86@yahoo.com>
2023-06-27 15:30:37 -0700

    Renders Ramen Images to the DOM

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-// write your code here
+// GET HTTP Request After the DOM Content has Loaded
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("http://localhost:3000/ramens")
+        .then(response => response.json())
+        .then(ramenData => renderRamen(ramenData))
+})
+
+// This function renders selected values from a mock database to the DOM
+function renderRamen(ramenData) {
+    const ramenMenu = document.querySelector("#ramen-menu")
+    ramenData.forEach(ramen => {
+        const img = document.createElement("img")
+        img.src = ramen.image
+        ramenMenu.appendChild(img)
+    });
+}
