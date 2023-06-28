65d7c9f7dac95b93b12d717757b82fe417f1df22
Yer Yang <yyhmg_86@yahoo.com>
2023-06-28 02:31:14 -0700

    Renders Ramen Details to the DOM

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
-// GET HTTP Request After the DOM Content has Loaded
-document.addEventListener("DOMContentLoaded", () => {
-    fetch("http://localhost:3000/ramens")
-        .then(response => response.json())
-        .then(ramenData => renderRamen(ramenData))
-})
+// Get data from our the mock database
+fetch("http://localhost:3000/ramens")
+    .then(response => response.json())
+    .then(ramenData => renderRamens(ramenData))
 
-// This function renders selected values from a mock database to the DOM
-function renderRamen(ramenData) {
+// This function iterate through the ramen data
+function renderRamens(ramenData) {
+    ramenData.forEach(renderRamen);
+}
+
+// This function grabs a dom element, creates an image, sets id and image source to image tag, renders image to the div element with an id of ramen-menu, and add event listener to image
+function renderRamen(ramen) {
     const ramenMenu = document.querySelector("#ramen-menu")
-    ramenData.forEach(ramen => {
-        const img = document.createElement("img")
-        img.src = ramen.image
-        ramenMenu.appendChild(img)
-    });
+    const img = document.createElement("img")
+    img.id = ramen.id
+    img.src = ramen.image
+    ramenMenu.appendChild(img)
+    img.addEventListener("click", event => renderRamenDetails(ramen))
+}
+
+// This function grabs dom elements and assign each to a specific value from ramen data
+function renderRamenDetails(ramen) {
+    const detailImage = document.querySelector(".detail-image")
+    detailImage.src = ramen.image
+    detailImage.alt = ramen.name
+    const ramenName = document.querySelector(".name")
+    ramenName.textContent = ramen.name
+    const restaurantName = document.querySelector(".restaurant")
+    restaurantName.textContent = ramen.restaurant
+    const ratingDisplay = document.querySelector("#rating-display")
+    ratingDisplay.textContent = ramen.rating
+    const commentDisplay = document.querySelector("#comment-display")
+    commentDisplay.textContent = ramen.comment
 }
